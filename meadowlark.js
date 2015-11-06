64190b0cda078efb89dcc1e10adbe21369f4c2f2
thestarcash <thestarcash@macbay.de>
2015-11-06 19:16:47 +0100

    first steps of using express

--- meadowlark.js
+++ meadowlark.js
@@ -1,32 +1,54 @@
-var express = require('express');
-
+var express = require ('express');
 var app = express();
 
-var fortune = ('./lib/fortune.js');
+//set up handlebars view engine
+var handlebars  = require ('express-handlebars').create({ defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+//end set up
 
-app.get('./about', function(get, res){
-	res.render('about', {fortune: fortune.getFortune()
-	});
+//add static middleware
+app.use(express.static(__dirname + '/public'));
+
+//to test dynamic content
+var fortunes = [
+  "blaBla",
+  "fdfsfsdfs",
+  "sdsdsdsdsds"
+]
+
+app.set('port', process.env.port || 8080);
+console.log('port 8888 set to app');
+
+app.get('/', function(req, res){
+  res.render('home');
+
+} );
+console.log('1');
+
+app.get('/about', function(req, res){
+  var fortuneRandom = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about', { fortune: fortuneRandom});
+});
+console.log('2');
+
+//custom 404 page
+app.use(function(req, res, next){
+  res.status(404);
+  res.render('404');
+});
+console.log('3');
+//app.use = using middleware
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+
+  res.status(500);
+  res.render('500');
 });
 
-
-app.set('port', process.env.PORT || 3000);
-
-
-app.use(function(req, res){
-	res.type('text/plain')
-	res.status(404);
-	res.send('404');
-});
-
-// 500 error handler (middleware)
-app.use(function(err, req, res, next){
-	console.error(err.stack);
-	res.status(500);
-	res.send('500');
-});
+console.log('4');
 
 app.listen(app.get('port'), function(){
-  console.log( 'Express started on http://localhost:' +
-    app.get('port') + '; press Ctrl-C to terminate.' );
+  console.log('5');
+  console.log( 'Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
 });
